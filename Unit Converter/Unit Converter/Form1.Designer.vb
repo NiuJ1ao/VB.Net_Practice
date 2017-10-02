2632b6b637f44c5ca47ce8096acc6cfee4f9dcad
NiuJ1ao <niuyuchen666@sina.com>
2017-10-02 16:40:37 +0100

    VB Windows Application - AS Project

---
+++ Unit Converter/Unit Converter/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("STHupo", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button2.Location = New System.Drawing.Point(281, 138)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(55, 46)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "---->" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "<----"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("SimSun", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(380, 217)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(175, 36)
+        Me.TextBox1.TabIndex = 15
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.Font = New System.Drawing.Font("SimSun", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.ImeMode = System.Windows.Forms.ImeMode.AlphaFull
+        Me.ComboBox2.ItemHeight = 19
+        Me.ComboBox2.Location = New System.Drawing.Point(380, 246)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(175, 27)
+        Me.ComboBox2.Sorted = True
+        Me.ComboBox2.TabIndex = 14
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("SimSun", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(61, 217)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(176, 36)
+        Me.TextBox2.TabIndex = 13
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.BackColor = System.Drawing.Color.White
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.Font = New System.Drawing.Font("SimSun", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"Speed", "Energy", "Mass"})
+        Me.ComboBox3.Location = New System.Drawing.Point(251, 67)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 33)
+        Me.ComboBox3.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("SimSun-ExtB", 40.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(282, 205)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 68)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "="
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("SimSun", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(61, 246)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(176, 27)
+        Me.ComboBox1.Sorted = True
+        Me.ComboBox1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(627, 361)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Unit Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Unit Converter/Unit Converter/Form1.vb
@@ -0,0 +1,401 @@
+﻿Public Module GlobalVariables
+    'Speed Unit
+    Public mpsTOkmph As Decimal = 3.6
+    Public mpsTOmlph As Decimal = 2.236936
+    Public mpsTOfpm As Decimal = 196.850394
+    Public mlpsTOkmph As Decimal = 5793.6384
+    Public mlpsTOmlph As Decimal = 3600
+    Public mlpsTOfpm As Decimal = 316800
+    Public fpsTOkmps As Decimal = 1.09728
+    Public fpsTOmlph As Decimal = 0.681818
+    Public fpsTOfpm As Decimal = 60
+
+    Public kmphTOmps As Decimal = 0.277778
+    Public mlphTOmps As Decimal = 0.44704
+    Public fpmTOmps As Decimal = 0.00508
+    Public kmphTOmlps As Decimal = 0.000173
+    Public mlphTOmlps As Decimal = 0.000278
+    Public fpmTOmlps As Decimal = 0.000003
+    Public kmphTOfps As Decimal = 0.911344
+    Public mlphTOfps As Decimal = 1.466667
+    Public fpmTOfps As Decimal = 0.016667
+
+    'Energy Unit
+    Public joulesTOfcalories As Decimal = 0.000239
+    Public joulesTOeV As Decimal = 6241457005723417000
+    Public joulesTONm As Decimal = 1
+    Public caloriesTOfcalories As Decimal = 0.001
+    Public caloriesTOeV As Decimal = 2.6131952590564E+19
+    Public caloriesTONm As Decimal = 4.1868
+    Public kwhTOfcalories As Decimal = 859845.227859
+    Public kwhTOeV As Decimal = 2.24692452206043E+25
+    Public kwhTONm As Decimal = 3600000
+
+    Public fcaloriesTOjoules As Decimal = 4186
+    Public eVTOjoules As Decimal = 1.602176487E-19
+    Public NmTOjoules As Decimal = 1
+    Public fcaloriesTOcalories As Decimal = 999.808923
+    Public eVTOcalories As Decimal = 3.8267327959301E-20
+    Public NmTOcalories As Decimal = 0.238846
+    Public fcaloriesTOkwh As Decimal = 0.000001
+    Public eVTOkwh As Decimal = 4.4504902416667E-26
+    Public NmTOkwh As Decimal = 0.000000278
+
+    'Mass Unit
+    Public gTOkg As Decimal = 0.001
+    Public gTOounces As Decimal = 0.035274
+    Public gTOslugs As Decimal = 0.000069
+    Public poundsTOkg As Decimal = 0.453592
+    Public poundsTOounces As Decimal = 16
+    Public poundsTOslugs As Decimal = 0.031081
+    Public tonsTOkg As Decimal = 1016.046909
+    Public tonsTOounces As Decimal = 35840
+    Public tonsTOslugs As Decimal = 69.621328
+
+    Public kgTOg As Decimal = 1000
+    Public ouncesTOg As Decimal = 28.349523
+    Public slugsTOg As Decimal = 14593.903
+    Public kgTOpounds As Decimal = 2.204623
+    Public ouncesTOpounds As Decimal = 0.0625
+    Public slugsTOpounds As Decimal = 32.174049
+    Public kgTOtons As Decimal = 0.000984
+    Public ouncesTOtons As Decimal = 0.000028
+    Public slugsTOtons As Decimal = 0.014363
+
+End Module
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox1.ReadOnly = True
+    End Sub
+    Public Sub SpeedConverter()
+        Dim StartingUnit As String = ComboBox1.Text
+        Dim DestinationUnit As String = ComboBox2.Text
+        Dim input As Decimal
+        Dim result As Decimal
+
+        'convert string to integer
+        input = CDec(TextBox2.Text)
+
+        'Calculation
+        Select Case StartingUnit
+            Case "meters per second"
+                Select Case DestinationUnit
+                    Case "kilometers per hour"
+                        result = input * mpsTOkmph
+                    Case "miles per hour"
+                        result = input * mpsTOmlph
+                    Case "feet per minute"
+                        result = input * mpsTOfpm
+                End Select
+            Case "miles per second"
+                Select Case DestinationUnit
+                    Case "kilometers per hour"
+                        result = input * mlpsTOkmph
+                    Case "miles per hour"
+                        result = input * mlpsTOmlph
+                    Case "feet per minute"
+                        result = input * mlpsTOfpm
+                End Select
+            Case "feet per second"
+                Select Case DestinationUnit
+                    Case "kilometers per hour"
+                        result = input * fpsTOkmps
+                    Case "miles per hour"
+                        result = input * fpsTOmlph
+                    Case "feet per minute"
+                        result = input * fpsTOfpm
+                End Select
+            Case "kilometers per hour"
+                Select Case DestinationUnit
+                    Case "meters per second"
+                        result = input * kmphTOmps
+                    Case "miles per second"
+                        result = input * kmphTOmlps
+                    Case "feet per second"
+                        result = input * kmphTOfps
+                End Select
+            Case "miles per hour"
+                Select Case DestinationUnit
+                    Case "meters per second"
+                        result = input * mlphTOmps
+                    Case "miles per second"
+                        result = input * mlphTOmlps
+                    Case "feet per second"
+                        result = input * mlphTOfps
+                End Select
+            Case "feet per minute"
+                Select Case DestinationUnit
+                    Case "meters per second"
+                        result = input * fpmTOmps
+                    Case "miles per second"
+                        result = input * fpmTOmlps
+                    Case "feet per second"
+                        result = input * fpmTOfps
+                End Select
+        End Select
+        TextBox1.Text = result
+
+    End Sub
+    Public Sub EnergyConverter()
+        Dim StartingUnit As String = ComboBox1.Text
+        Dim DestinationUnit As String = ComboBox2.Text
+        Dim input As Decimal
+        Dim result As Decimal
+
+        'convert string to integer
+        input = CDec(TextBox2.Text)
+
+        'Calculation
+        Select Case StartingUnit
+            Case "joules"
+                Select Case DestinationUnit
+                    Case "calories(food)"
+                        result = input * joulesTOfcalories
+                    Case "electron volts"
+                        result = input * joulesTOeV
+                    Case "newton meters"
+                        result = input * joulesTONm
+                End Select
+            Case "calories"
+                Select Case DestinationUnit
+                    Case "calories(food)"
+                        result = input * caloriesTOfcalories
+                    Case "electron volts"
+                        result = input * caloriesTOeV
+                    Case "newton meters"
+                        result = input * caloriesTONm
+                End Select
+            Case "kilowatt hours"
+                Select Case DestinationUnit
+                    Case "calories(food)"
+                        result = input * kwhTOfcalories
+                    Case "electron volts"
+                        result = input * kwhTOeV
+                    Case "newton meters"
+                        result = input * kwhTONm
+                End Select
+            Case "calories(food)"
+                Select Case DestinationUnit
+                    Case "joules"
+                        result = input * fcaloriesTOjoules
+                    Case "calories"
+                        result = input * fcaloriesTOcalories
+                    Case "kilowatt hours"
+                        result = input * fcaloriesTOkwh
+                End Select
+            Case "electron volts"
+                Select Case DestinationUnit
+                    Case "joules"
+                        result = input * eVTOjoules
+                    Case "calories"
+                        result = input * eVTOcalories
+                    Case "kilowatt hours"
+                        result = input * eVTOkwh
+                End Select
+            Case "newton meters"
+                Select Case DestinationUnit
+                    Case "joules"
+                        result = input * NmTOjoules
+                    Case "calories"
+                        result = input * NmTOcalories
+                    Case "kilowatt hours"
+                        result = input * NmTOkwh
+                End Select
+        End Select
+        TextBox1.Text = result
+
+    End Sub
+    Public Sub MassConverter()
+        Dim StartingUnit As String = ComboBox1.Text
+        Dim DestinationUnit As String = ComboBox2.Text
+        Dim input As Decimal
+        Dim result As Decimal
+
+        'convert string to integer
+        input = CDec(TextBox2.Text)
+
+        'Calculation
+        Select Case StartingUnit
+            Case "grams"
+                Select Case DestinationUnit
+                    Case "kilograms"
+                        result = input * gTOkg
+                    Case "ounces"
+                        result = input * gTOounces
+                    Case "slugs"
+                        result = input * gTOslugs
+                End Select
+            Case "pounds"
+                Select Case DestinationUnit
+                    Case "kilograms"
+                        result = input * poundsTOkg
+                    Case "ounces"
+                        result = input * poundsTOounces
+                    Case "slugs"
+                        result = input * poundsTOslugs
+                End Select
+            Case "tons"
+                Select Case DestinationUnit
+                    Case "kilograms"
+                        result = input * tonsTOkg
+                    Case "ounces"
+                        result = input * tonsTOounces
+                    Case "slugs"
+                        result = input * tonsTOslugs
+                End Select
+            Case "kilograms"
+                Select Case DestinationUnit
+                    Case "grams"
+                        result = input * kgTOg
+                    Case "pounds"
+                        result = input * kgTOpounds
+                    Case "tons"
+                        result = input * kgTOtons
+                End Select
+            Case "ounces"
+                Select Case DestinationUnit
+                    Case "grams"
+                        result = input * ouncesTOg
+                    Case "pounds"
+                        result = input * ouncesTOpounds
+                    Case "tons"
+                        result = input * ouncesTOtons
+                End Select
+            Case "slugs"
+                Select Case DestinationUnit
+                    Case "grams"
+                        result = input * slugsTOg
+                    Case "pounds"
+                        result = input * slugsTOpounds
+                    Case "tons"
+                        result = input * slugsTOtons
+                End Select
+        End Select
+        TextBox1.Text = result
+
+    End Sub
+    Private Sub executepart()
+        Dim Number As Boolean = True
+
+        If TextBox2.Text = "" Then
+            TextBox1.Text = ""
+        Else
+            Number = check()
+            If Number = False Then
+                MessageBox.Show("Invalid Character, Please enter a number")
+            Else
+                Select Case ComboBox3.Text()
+                    Case "Speed"
+                        SpeedConverter()
+                    Case "Energy"
+                        EnergyConverter()
+                    Case "Mass"
+                        MassConverter()
+                End Select
+            End If
+        End If
+    End Sub
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        executepart()
+    End Sub
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        executepart()
+    End Sub
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        executepart()
+    End Sub
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        Dim SpeedUnit1() As String = {"meters per second", "miles per second", "feet per second"}
+        Dim SpeedUnit2() As String = {"kilometers per hour", "miles per hour", "feet per minute"}
+        Dim EnergyUnit1() As String = {"joules", "calories", "kilowatt hours"}
+        Dim EnergyUnit2() As String = {"calories(food)", "electron volts", "newton meters"}
+        Dim MassUnit1() As String = {"grams", "pounds", "tons"}
+        Dim MassUnit2() As String = {"kilograms", "ounces", "slugs"}
+
+        If ComboBox3.Text = "" Then
+            ComboBox1.Items.Clear()
+            ComboBox2.Items.Clear()
+        Else
+            Select Case ComboBox3.SelectedItem
+                Case "Speed"
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                    ComboBox1.Items.Clear()
+                    ComboBox2.Items.Clear()
+                    ComboBox1.Items.AddRange(SpeedUnit1)
+                    ComboBox2.Items.AddRange(SpeedUnit2)
+                Case "Energy"
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                    ComboBox1.Items.Clear()
+                    ComboBox2.Items.Clear()
+                    ComboBox1.Items.AddRange(EnergyUnit1)
+                    ComboBox2.Items.AddRange(EnergyUnit2)
+                Case "Mass"
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                    ComboBox1.Items.Clear()
+                    ComboBox2.Items.Clear()
+                    ComboBox1.Items.AddRange(MassUnit1)
+                    ComboBox2.Items.AddRange(MassUnit2)
+            End Select
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Num1 As Integer = ComboBox1.Items.Count - 1
+        Dim Num2 As Integer = ComboBox2.Items.Count - 1
+        Dim Items1(Num1) As String
+        Dim items2(Num2) As String
+        Dim i As Integer
+        Dim j As Integer
+        Dim CurrentItem1 As String = ComboBox1.SelectedItem
+        Dim CurrentItem2 As String = ComboBox2.SelectedItem
+
+        'Get all items from combobox1
+        For i = 0 To Num1
+            Items1(i) = ComboBox1.Items(i)
+        Next
+
+        'Get all items from combobox2
+        For j = 0 To Num2
+            items2(j) = ComboBox2.Items(j)
+        Next
+
+        ComboBox1.Items.Clear()
+        ComboBox2.Items.Clear()
+
+        For i = 0 To Num1
+            ComboBox2.Items.Add(Items1(i))
+        Next
+
+        For j = 0 To Num2
+            ComboBox1.Items.Add(items2(j))
+        Next
+
+        ComboBox1.Text = CurrentItem2
+        ComboBox2.Text = CurrentItem1
+
+    End Sub
+    Public Function check() As Boolean
+        Dim i As Integer = TextBox2.Text.Count - 1
+        Dim NumArray() As Char
+        Dim value As Integer
+        Dim correct As Boolean = True
+
+        NumArray = TextBox2.Text.ToCharArray
+
+        For i = 0 To i
+            value = Asc(NumArray(i))
+            If value < 47 Or value > 58 Then
+                correct = False
+            End If
+
+            If value = 46 Then
+                correct = True
+            End If
+        Next
+
+        Return correct
+
+    End Function
+End Class
